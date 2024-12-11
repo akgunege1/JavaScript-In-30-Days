8fd6a38bb4b324b490f9d40c470d5b5cf0b95ce0
akgunege1 <233301082@ogr.selcuk.edu.tr>
2024-12-11 16:39:00 +0300

    Day 6

---
+++ Day-6/scripts.js
@@ -0,0 +1,6 @@
+//Aşık olmak anlık bir şey. 
+//Birden her şeyin çok parlak göründüğü.
+//Birden en pastel renklerin ısınmaya başladığı.
+//Birden tüm yemeklerin çok daha lezzetli olduğu bir an bu.
+//İnsan karar vererek aşık olmaz.
+//Sadece bir bakar olmuş.
